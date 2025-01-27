fd6b69a28290922853a4aa736e19b02e8ca63d01
ZyX <161449498+GossuZyX@users.noreply.github.com>
2025-01-27 12:31:09 +0100

    Update do_backup.sh
    Added custom namespace param

--- do_backup.sh
+++ do_backup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 CUSTOM_HOST_STRING=""
+CUSTOM_NAMESPACE_STRING=""
 if [ ! -z "$CUSTOM_HOST" ]; then
     CUSTOM_HOST_STRING="--backup-id $CUSTOM_HOST"
 fi
 
+if [ ! -z "$CUSTOM_NAMESPACE" ]; then
+    CUSTOM_NAMESPACE_STRING="--ns $CUSTOM_NAMESPACE"
+fi
+
 echo "------------------------------------------------------------"
-proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING
+proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING $CUSTOM_NAMESPACE_STRING
 echo "------------------------------------------------------------"
