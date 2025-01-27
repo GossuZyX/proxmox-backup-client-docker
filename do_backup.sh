3d3be6bed535ac05e846a6c71b8fcecba78889f7
ZyX <161449498+GossuZyX@users.noreply.github.com>
2025-01-27 15:40:21 +0100

    Update do_backup.sh
    added excluded directories option

--- do_backup.sh
+++ do_backup.sh
@@ -2,6 +2,7 @@
 
 CUSTOM_HOST_STRING=""
 CUSTOM_NAMESPACE_STRING=""
+CUSTOM_EXCLUDED_DIRECTORIES_STRING=""
 if [ ! -z "$CUSTOM_HOST" ]; then
     CUSTOM_HOST_STRING="--backup-id $CUSTOM_HOST"
 fi
@@ -10,6 +11,25 @@ if [ ! -z "$NAMESPACE" ]; then
     CUSTOM_NAMESPACE_STRING="--ns $NAMESPACE"
 fi
 
+if [ ! -z "$EXCLUDED_DIRECTORIES" ]; then
+    CUSTOM_EXCLUDED_DIRECTORIES_STRING=""
+    
+    # Split EXCLUDED_DIRECTORIES by comma
+    IFS=',' read -ra DIR_ARRAY <<< "$EXCLUDED_DIRECTORIES"
+    
+    for DIR in "${DIR_ARRAY[@]}"; do
+        # Trim spaces and check if directory exists
+        DIR=$(echo "$DIR" | xargs)
+        if [ -d "$DIR" ]; then
+            CUSTOM_EXCLUDED_DIRECTORIES_STRING+="--exclude $DIR "
+        fi
+    done
+    
+    # Remove trailing space
+    CUSTOM_EXCLUDED_DIRECTORIES_STRING=$(echo "$CUSTOM_EXCLUDED_DIRECTORIES_STRING" | xargs)
+fi
+
+
 echo "------------------------------------------------------------"
-proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING $CUSTOM_NAMESPACE_STRING
+proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_EXCLUDED_DIRECTORIES_STRING $CUSTOM_HOST_STRING $CUSTOM_NAMESPACE_STRING
 echo "------------------------------------------------------------"
